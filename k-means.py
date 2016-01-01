8c603ecf9b64f4d560995cfd4f44b4f6b09a6156
salamer <salameryy@163.com>
2016-01-01 20:55:47 +0800

    ~~~

---
+++ k-means.py
@@ -0,0 +1,57 @@
+from numpy import *
+from numpy.random.mtrand import power
+
+def loadDataSet(fileName):
+	dataMat=[]
+	fr=open(fileName)
+	for line in fr.readlines():
+		curLine=line.strip().split('\t')
+		fitLine=map(float,curLine)
+		dataMat.append(fitLine)
+	return dataMat
+
+def distEclud(vecA,vecB):
+	return sqrt(sum((vecA-vecB)**2))
+
+def randCent(dataSet,k):
+	n=shape(dataSet)[1]
+	centroids=mat(zeros((k,n)))
+	for j in range(n):
+		minJ=min(dataSet[:,j])
+		rangeJ=float(max(dataSet[:,j]-minJ))
+		centroids[:,j]=minJ+rangeJ*random.rand(k,1)
+	return centroids
+
+def kMeans(dataSet,k,distMeas=distEclud,createCent=randCent):
+	m=shape(dataSet)[0]
+	clusterAssment=mat(zeros((m,2)))
+	centroids=createCent(dataSet,k)
+	clusterChanged=True
+	while clusterChanged:
+		clusterChanged=False
+		for i in range(m):
+			minDist=inf
+			minIndex=-1
+			for j in range(k):
+				distJI=distMeas(centroids[j,:],dataSet[i,:])
+				if distJI<minDist:
+					minDist=distJI
+					minIndex=j
+			if clusterAssment[i,0]!=minIndex:
+				clusterChanged=True
+			clusterAssment[i,:]=minIndex,minDist**2
+
+		print(centroids)
+		
+		for cent in range(k):
+			ptInClust=dataSet[nonzero(clusterAssment[:,0].A==cent)[0]]
+			centroids[cent,:]=mean(ptInClust,axis=0)
+
+	return centroids,clusterAssment	
+
+if __name__=="__main__":
+	dataMat=mat(loadDataSet('testSet.txt'))
+	myCentoids,clusterAssing=kMeans(dataMat,4)
+	print "myCentoids is %s"%myCentoids
+	print "\n"
+	print "clusterAssing is %s "%clusterAssing
