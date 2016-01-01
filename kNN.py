f6326c7762d0dbea12302b9a50ebccd0c72fd187
salamer <salameryy@163.com>
2016-01-01 19:40:57 +0800

    ~~~

--- kNN.py
+++ kNN.py
@@ -1,3 +1,11 @@
+'''
+author:aljun
+date:20160101
+from:<machine learnning in action> chapter 2
+
+'''
+
+
 from numpy import *
 
 import operator
@@ -22,3 +30,80 @@ def classify0(inX,dataSet,labels,k):
 
 	sortedClassCount=sorted(classCount.iteritems(),key=operator.itemgetter(1),reverse=True)
 	return sortedClassCount[0][0]
+
+def file2matrix(filename):
+	fr=open(filename)
+	arrayOLines=fr.readlines()
+	numerOfLines=len(arrayOLines)
+	returnMat=zeros((numberOfLines,3))
+	classLabelVector=[]
+	index=0
+	for line in arrayOLines:
+		line=line.strip()
+		listFromLine=line.split('\t')
+		returnMat[index,:]=listFromLine[0:3]
+		classLabelVector.append(int(listFromLine[-1]))
+		index+=1
+
+	return returnMat,classLabelVector
+
+def autoNorm(dataSet):
+	minVals=dataSet.min(0)
+	maxVals=dataSet.max(0)
+	ranges=maxVals-minVals
+	normDataSet=zeros(shape(dataSet))
+	m=dataSet.shape[0]
+	normDataSet=dataSet-tile(minVals,(m,1))
+	normDataSet=normDataSet/tile(ranges,(m,1))
+	return normDataSet,ranges,minVals
+
+
+
+'''
+手写系统
+
+'''
+
+def img2vector(filename):
+	returnVect=zeros((1,1024))
+	fr=open(filename)
+	for i in range(32):
+		lineStr=fr.readline()
+		for j in range(32):
+			returnVect[0,32*i+j]=int(lineStr[j])
+	return returnVect
+
+
+
+def handwritingClasstest():
+	hwLabels=[]
+	trainingFileList=listdir('trainingDigits')
+	m=len(trainingFileList)
+	tariningMat=zeros((m,1024))
+	for i in range(m):
+		fileNameStr=trainingFileList[i]
+		fileStr=fileNameStr.split('.')[0]
+		classNumber=int(fileStr.split('_')[0])
+		hwLabels.append(classNumber)
+		tariningMat[i,:]=img2vector('trainingDigits/%s' % fileNameStr)
+	testFileList=listdir('testDigits')
+	errorCount=0.0
+	mTest=len(testFileList)
+	for i in range(mTest):
+		fileNameStr=testFileList[i]
+		fileStr=fileNameStr.split('.')[0]
+		classNumberStr=int(fileStr.split('_')[0])
+		vectorUnderTest=img2vector('testDigits/%s'%fileNameStr)
+		classifierResult=classify0(vectorUnderTest,tariningMat,hwLabels,3)
+		print "real answer is %d" % (classifierResult,classNumStr)
+		if(classifierResult!=classNumStr):
+			errorCount+=1.0
+	print "error is %s"%errorCount
+
+
+
+
+
+
+
+
